41dfd0dd64dde1fc70a678110aaa874181a4a45c
MrBigRedButton <73118706+MrBigRedButton@users.noreply.github.com>
2024-05-08 12:13:23 +0200

    added a hash functionality to login (kinda buggy, needs a little more work)

--- Services/Login.cs
+++ Services/Login.cs
@@ -3,6 +3,9 @@ using MTG.Models;
 using static MTG.Services.Services;
 using System.Drawing;
 using System;
+using System.Security.Cryptography;
+using System.Linq;
+using System.Text;
 
 namespace MTG.Services
 {
@@ -37,6 +40,48 @@ namespace MTG.Services
                 return false;
             }
         }
+        // attempt to hash passwords
 
+        private const int SaltSize = 16; // 128 bit 
+        private const int KeySize = 32; // 256 bit
+
+        public static bool ValidatePassword(User user, string storedSalt)
+        {
+            User userAttempt = dbContext.Users.FirstOrDefault(u => u.Username == user.Username);
+            if (userAttempt != null)
+            {
+                var saltBytes = Convert.FromBase64String(storedSalt);
+                var rfc2898DeriveBytes = new Rfc2898DeriveBytes(user.Password, saltBytes, 10000);
+                return Convert.ToBase64String(rfc2898DeriveBytes.GetBytes(KeySize)) == userAttempt.Password;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static void CreatePasswordHash(User user)
+        {
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                var saltBytes = new byte[SaltSize];
+                rng.GetBytes(saltBytes);
+
+                var rfc2898DeriveBytes = new Rfc2898DeriveBytes(user.Password, saltBytes, 10000);
+                var salt = Convert.ToBase64String(saltBytes);
+                var hash = Convert.ToBase64String(rfc2898DeriveBytes.GetBytes(KeySize));
+
+                var attemptedUser = dbContext.Users.FirstOrDefault(u => u.Username == user.Username);
+                if (attemptedUser == null)
+                {
+                    dbContext.Users.Add(user);
+                }
+
+                user.Password = hash;
+                user.Salt = salt;
+
+                dbContext.SaveChanges();
+            }
+        }
     }
 }
