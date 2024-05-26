3c030baa1f51060de898e82da1bba1b244e3d036
Piotr Skupinski <krogulew@gmail.com>
2024-05-26 21:40:28 +0200

    Final random fix

--- Components/Pages/RandomCard.razor.cs
+++ Components/Pages/RandomCard.razor.cs
@@ -20,8 +20,25 @@ namespace MTG.Components.Pages
             base.OnAfterRender(firstRender);
             if (firstRender)
             {
-                NavigationManager.NavigateTo($"/Card/{randomCard.First().Name}");
+              await GetRandomCard();
             }
+        }
+        protected async Task GetRandomCard()
+        {
+            
+                
+                while (randomCard==null)
+                {
+                    int rnd = new Random().Next(1, 5000);
+                    randomCard = services.GetCardById(rnd);
+                    Console.WriteLine("Random card: "+rnd);
+                    if(randomCard.First().Image==null) randomCard=null;
+                }
+            try { NavigationManager.NavigateTo($"/Card/{randomCard.First().Name}"); } 
+            catch { 
+                Console.WriteLine("Error: Card not found");
+                NavigationManager.NavigateTo($"/Card/Counterspell"); }
+
         }
 
     }
